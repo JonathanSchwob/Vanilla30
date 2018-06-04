9d4f231eee20aec70d38b576f1c062344988f139
JSchwo <Jonathan.Schwob@nike.com>
2018-06-04 16:06:15 -0700

    init

---
+++ JS - 01/script.js
@@ -0,0 +1,74 @@
+document.addEventListener("keydown", function(e) {
+  if (e.keyCode == 65) {
+    let audio = document.getElementById("a");
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.currentTime = 0;
+    }
+  }
+  if (e.keyCode == 83) {
+    let audio = document.getElementById("s");
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.currentTime = 0;
+    }
+  }
+  if (e.keyCode == 68) {
+    let audio = document.getElementById("d");
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.currentTime = 0;
+    }
+  }
+  if (e.keyCode == 70) {
+    let audio = document.getElementById("f");
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.currentTime = 0;
+    }
+  }
+  if (e.keyCode == 71) {
+    let audio = document.getElementById("g");
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.currentTime = 0;
+    }
+  }
+  if (e.keyCode == 72) {
+    let audio = document.getElementById("h");
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.currentTime = 0;
+    }
+  }
+  if (e.keyCode == 74) {
+    let audio = document.getElementById("j");
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.currentTime = 0;
+    }
+  }
+  if (e.keyCode == 75) {
+    let audio = document.getElementById("k");
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.currentTime = 0;
+    }
+  }
+  if (e.keyCode == 76) {
+    let audio = document.getElementById("l");
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.currentTime = 0;
+    }
+  }
+});
